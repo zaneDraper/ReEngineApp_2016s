7bbf74a10897b744c491f14b5621b65d02c63bb9
unknown <sarathijh@gmail.com>
2017-03-23 18:14:37 -0400

    E11 finished

--- E11_MidtermPractice/AppClass.cpp
+++ E11_MidtermPractice/AppClass.cpp
@@ -10,12 +10,35 @@ void AppClass::Update(void)
 #pragma endregion
 #pragma region FEEL FREE TO USE THIS CLOCK
 	//Calculate delta and total times
-	static double dTotalTime = 0.0; //Total time of the simulation
-	double dDeltaTime = m_pSystem->LapClock(); //time difference between function calls
+	static float dTotalTime = 0.0; //Total time of the simulation
+	float dDeltaTime = m_pSystem->LapClock(); //time difference between function calls
 	dTotalTime += dDeltaTime; //Incrementing the time differences 
 #pragma endregion
 #pragma region YOUR CODE GOES HERE
-	m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
+
+	//interpelation for rotation and translation
+	static float startLerp = 0.0;
+	static float endLerp = 5.0;
+	float fPercent = MapValue(dTotalTime, startLerp, endLerp, 0.0f, 1.0f);
+	float rotation = MapValue(dTotalTime, startLerp, endLerp, 0.0f, 360.0f);
+
+	//keep the translation from moving too far out
+	if (fPercent >= 2) {
+		fPercent = 0.0f;
+		startLerp = dTotalTime;
+		endLerp = startLerp + 5.0f;
+	}
+	//starts reducing the distance of the translation
+	else if (fPercent >= 1) {
+		float temp = fPercent - 1;
+		fPercent = 1 - temp;
+	}
+
+	//set the rotation and translation matrix
+	matrix4 rotMat = glm::rotate(IDENTITY_M4, rotation, vector3(0, 0, 1));
+	matrix4 distance = glm::translate(0.0f, glm::lerp(0.0f, 5.0f, fPercent), 0.0f);
+
+	m_m4Steve = rotMat * distance;
 #pragma endregion
 #pragma region DOES NOT NEED CHANGES
 	//Set the model matrix
