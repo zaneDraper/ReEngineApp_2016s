a52eb22933fa1c371910287b9442322555be2804
Zane Draper <ztd9457@g.rit.edu>
2017-04-05 02:05:05 -0400

    A08 almost finish - still needs  SetUp/SetTarget

---
+++ A08_Camera/AppClass.cpp
@@ -0,0 +1,82 @@
+#include "AppClass.h"
+void AppClass::InitWindow(String a_sWindowName)
+{
+	super::InitWindow("Zane - A08 Camera"); // Window Name
+
+	m_v4ClearColor = vector4(0.4f, 0.6f, 0.9f, 0.0f);
+	m_pSystem->SetWindowResolution(RESOLUTIONS::C_1280x720_16x9_HD);
+}
+
+void AppClass::InitVariables(void)
+{
+	camera.SetPosition(vector3(10.f, 4.f, -7.f));
+	camera.SetTarget(vector3(0.f, 0.f, 0.f));
+
+	//Generate the Cone
+	m_pCone = new PrimitiveClass();
+	m_pCone->GenerateCone(1.0f, 1.0f, 10, RERED);
+
+	//Generate the Cylinder
+	m_pCylinder = new PrimitiveClass();
+	m_pCylinder->GenerateCylinder(1.0f, 1.0f, 10, REGREEN);
+
+}
+
+void AppClass::Update(void)
+{
+	//Update the system's time
+	m_pSystem->UpdateTime();
+
+	//Update the mesh manager's time without updating for collision detection
+	m_pMeshMngr->Update();
+
+	//First person camera movement
+	if (m_bFPC == true)
+		CameraRotation();
+
+	//Call the arcball method
+	ArcBall();
+	
+	//Set the model matrix for the first model to be the arcball
+	m_pMeshMngr->SetModelMatrix(ToMatrix4(m_qArcBall), 0);
+	
+	//Adds all loaded instance to the render list
+	m_pMeshMngr->AddSkyboxToRenderList();
+	m_pMeshMngr->AddInstanceToRenderList("ALL");
+
+	//Indicate the FPS
+	int nFPS = m_pSystem->GetFPS();
+	//print info into the console
+	//printf("FPS: %d            \r", nFPS);//print the Frames per Second
+	//Print info on the screen
+	m_pMeshMngr->PrintLine(m_pSystem->GetAppName(), REYELLOW);
+
+	m_pMeshMngr->Print("Selection: ");
+	m_pMeshMngr->PrintLine(m_pMeshMngr->GetInstanceGroupName(m_selection.first, m_selection.second), REYELLOW);
+	
+	m_pMeshMngr->Print("FPS:");
+	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
+}
+
+void AppClass::Display(void)
+{
+	//clear the screen
+	ClearScreen();
+
+	//Render the cone
+	m_pCone->Render(camera.GetProjection(true), camera.GetView(), IDENTITY_M4);
+
+	//Render the cylinder
+	m_pCylinder->Render(camera.GetProjection(false), camera.GetView(), glm::translate(IDENTITY_M4, REAXISZ * -3.0f));
+
+	//Render the grid based on the camera's mode:
+	//m_pMeshMngr->AddGridToRenderListBasedOnCamera(m_pCameraMngr->GetCameraMode());
+	m_pMeshMngr->Render(); //renders the render list
+	m_pMeshMngr->ClearRenderList(); //Reset the Render list after render
+	m_pGLSystem->GLSwapBuffers(); //Swaps the OpenGL buffers
+}
+
+void AppClass::Release(void)
+{
+	super::Release(); //release the memory of the inherited fields
+}
---
+++ A08_Camera/CameraCustom.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "RE\ReEngAppClass.h"
+#include <SFML\Graphics.hpp>
+
+using namespace ReEng; //Using ReEng namespace to use all the classes in the dll
+
+class CameraCustom
+{
+
+	glm::quat rotation;
+	vector3 position;
+	vector3 forward;
+	vector3 right;
+	vector3 up;;
+	vector3 center;
+
+public:
+	CameraCustom();
+	~CameraCustom();
+
+	matrix4 GetView();
+
+	matrix4 GetProjection(bool bOrthographic);
+
+	void SetPosition(vector3 v3Position);
+
+	void SetTarget(vector3 v3Target);
+
+	void SetUp(vector3 v3Up);
+
+	void MoveForward(float fIncrement);
+
+	void MoveSideways(float fIncrement);
+
+	void MoveVertical(float fIncrement);
+
+	void ChangePitch(float fIncrement);
+
+	void ChangeRoll(float fIncrement);
+
+	void ChangeYaw(float fIncrement);
+
+	void UpdateDirectionVectors();
+};
+
